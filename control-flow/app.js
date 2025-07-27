1951bfaac59ca86526fcdee4b66cd23086011c2d
Debian Live user <user@localhost.localdomain>
2025-07-27 15:11:28 +0000

    second commit before online push

---
+++ control-flow/app.js
@@ -0,0 +1,11 @@
+console.log(10 == "10");
+
+const isGameOver = false;
+
+if (isGameOver == true){
+    console.log("Game Over")
+} else {
+    console.log("Continue")
+}
+
+console.log("End of file")
